9b9f3bcedd54a266c20ca8abe6daefb3a46b04b3
Dev <dev@Uross-MacBook-Pro-2.local>
2023-11-24 16:44:02 +0100

    new game and roll dice button events

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+// New Game Button Click
+document.getElementById("newGameBtn").addEventListener("click", function () {
+  document.getElementById("dice").style.visibility = "hidden";
+});
+
+// Roll Dice Button Click
+document.getElementById("rollDiceBtn").addEventListener("click", function () {
+  document.getElementById("dice").style.visibility = "visible";
+});
+
+// Hold Button Click
