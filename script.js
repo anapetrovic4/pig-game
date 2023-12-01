dd525ce9e651f182eb65e7014d6970e0ce7d035a
Ana Petrovic <petrovic.bs.ana@gmail.com>
2023-12-01 13:07:34 +0100

    add sum function implemented to current score

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 "use strict";
+//Global variables
+let randomNumber;
+let sum = 0;
+let currentScore = 0;
 
 // Generate random number between 1 and 6
 function generateRandomNumber() {
@@ -8,7 +12,7 @@ function generateRandomNumber() {
 //Set the dice image based on the random number
 function setDiceImage(randomNumber) {
   const diceImage = document.getElementById("diceImage");
-  
+
   //Update the source attribute of the dice image
   diceImage.src = `dice-${randomNumber}.png`;
 }
@@ -22,13 +26,38 @@ document.getElementById("newGameBtn").addEventListener("click", function () {
 document.getElementById("rollDiceBtn").addEventListener("click", function () {
   
   // Generate random number
-  const randomNumber = generateRandomNumber();
+  randomNumber = generateRandomNumber();
 
   //Set the dice image based on the random number
   setDiceImage(randomNumber);
 
   // Show the dice
   document.getElementById("dice").style.visibility = "visible";
+
+  addSum();
+
 });
+
+//Add sum
+function addSum(){
+  sum += randomNumber;
+  console.log( `The sum is: ${sum}`);
+
+  //If the random number is 1, reset the sum to 0
+
+  if(randomNumber !== 1){
+  currentScore = document.getElementById("current1");
+  currentScore.textContent = sum;
+  } else {
+    sum = 0;
+    currentScore.textContent = sum;
+  }
+
+}
+//Kad se klikne na hold, dodaj sumu na global score
+//Kad se klikne na new game, resetuj sve na 0
+//Kad se dodje do 100, prikazi pobednika i onemoguci roll dice i hold
+
+
 
 
