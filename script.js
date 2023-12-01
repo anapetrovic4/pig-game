4991d85db920d7a845a3ace00af77d4b57da563a
Ana Petrovic <petrovic.bs.ana@gmail.com>
2023-12-01 12:44:28 +0100

    set the dice image based on the random number functionality

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 "use strict";
 
+// Generate random number between 1 and 6
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+//Set the dice image based on the random number
+function setDiceImage(randomNumber) {
+  const diceImage = document.getElementById("diceImage");
+  
+  //Update the source attribute of the dice image
+  diceImage.src = `dice-${randomNumber}.png`;
+}
+
 // New Game Button Click
 document.getElementById("newGameBtn").addEventListener("click", function () {
   document.getElementById("dice").style.visibility = "hidden";
@@ -7,6 +20,14 @@ document.getElementById("newGameBtn").addEventListener("click", function () {
 
 // Roll Dice Button Click
 document.getElementById("rollDiceBtn").addEventListener("click", function () {
+  
+  // Generate random number
+  const randomNumber = generateRandomNumber();
+
+  //Set the dice image based on the random number
+  setDiceImage(randomNumber);
+
+  // Show the dice
   document.getElementById("dice").style.visibility = "visible";
 });
 
