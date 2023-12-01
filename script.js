e01659bfd3e64d85e06a0fece49622cbf43c3538
Ana Petrovic <petrovic.bs.ana@gmail.com>
2023-12-01 14:35:00 +0100

    set the global score of player 1 when pressed holdBtn

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+
 "use strict";
 //Global variables
 let randomNumber;
 let sum = 0;
 let currentScore = 0;
+let globalScorePlayer1 = 0;
+let globalScorePlayer2 = 0;
+let currentPlayer = 1;
 
 // Generate random number between 1 and 6
 function generateRandomNumber() {
@@ -20,6 +25,17 @@ function setDiceImage(randomNumber) {
 // New Game Button Click
 document.getElementById("newGameBtn").addEventListener("click", function () {
   document.getElementById("dice").style.visibility = "hidden";
+
+  globalScorePlayer1 = 0;
+  globalScorePlayer2 = 0;
+  currentScore = 0;
+
+  document.getElementById("current1").textContent = 0;
+  document.getElementById("score1").textContent = 0;
+  document.getElementById("current2").textContent = 0;
+  document.getElementById("score2").textContent = 0;
+
+
 });
 
 // Roll Dice Button Click
@@ -35,29 +51,48 @@ document.getElementById("rollDiceBtn").addEventListener("click", function () {
   document.getElementById("dice").style.visibility = "visible";
 
   addSum();
+});
+
+//Hold Dice Button Click
+document.getElementById('holdBtn').addEventListener('click', function(){
+  console.log('Hold dice button clicked');
+
+if(currentPlayer === 1){
+  globalScorePlayer1 += currentScore;
+  document.getElementById('score1').textContent = globalScorePlayer1;
+} else {
+  globalScorePlayer2 += currentScore;
+  document.getElementById('score2').textContent = globalScorePlayer2;
+}
+
+currentScore = 0;
+document.getElementById(`current${currentPlayer}`).textContent = currentScore;
+
+//switch player 
 
 });
 
 //Add sum
 function addSum(){
-  sum += randomNumber;
-  console.log( `The sum is: ${sum}`);
-
-  //If the random number is 1, reset the sum to 0
-
+  //If the random number is 1, reset the sum to 0 and switch players
   if(randomNumber !== 1){
-  currentScore = document.getElementById("current1");
-  currentScore.textContent = sum;
+    sum += randomNumber;
+    console.log( `The sum is: ${sum}`);
+    currentScore += randomNumber;
+    document.getElementById(`current${currentPlayer}`).textContent = currentScore;
   } else {
-    sum = 0;
-    currentScore.textContent = sum;
+    currentScore = 0;
+    document.getElementById(`current${currentPlayer}`).textContent = currentScore;
+    return;
   }
 
 }
-//Kad se klikne na hold, dodaj sumu na global score
+
+//Switch player
+//Kad se klikne na hold, dodaj currentScore na global score
 //Kad se klikne na new game, resetuj sve na 0
 //Kad se dodje do 100, prikazi pobednika i onemoguci roll dice i hold
 
-
+});
 
 
