e3ddbddd171e6c79470063b1c282601d3328a60c
Ana Petrovic <petrovic.bs.ana@gmail.com>
2023-12-04 13:59:49 +0100

    switch colors on hold button

--- script.js
+++ script.js
@@ -51,27 +51,45 @@ document.getElementById("rollDiceBtn").addEventListener("click", function () {
   document.getElementById("dice").style.visibility = "visible";
 
   addSum();
+
+
+
 });
 
 //Hold Dice Button Click
 document.getElementById('holdBtn').addEventListener('click', function(){
   console.log('Hold dice button clicked');
+  
+ if(currentPlayer === 1){
+    globalScorePlayer1 += currentScore;
+    document.getElementById('score1').textContent = globalScorePlayer1;
+  } else {
+    globalScorePlayer2 += currentScore;
+    document.getElementById('score2').textContent = globalScorePlayer2;
+  }
 
-if(currentPlayer === 1){
-  globalScorePlayer1 += currentScore;
-  document.getElementById('score1').textContent = globalScorePlayer1;
-} else {
-  globalScorePlayer2 += currentScore;
-  document.getElementById('score2').textContent = globalScorePlayer2;
-}
-
-currentScore = 0;
-document.getElementById(`current${currentPlayer}`).textContent = currentScore;
+  currentScore = 0;
+  document.getElementById(`current${currentPlayer}`).textContent = currentScore;
 
 //switch player 
 switchPlayer();
 
 });
+
+//Switch player
+function switchPlayer(){
+  currentPlayer = currentPlayer === 1 ? 2 : 1;
+// Ukloni klasu "active" sa trenutnog igrača
+document.querySelector(`.child${currentPlayer === 1 ? 2 : 1}`).classList.remove('active');
+document.querySelector(`.child${currentPlayer === 1 ? 2 : 1}`).classList.add('notactive');
+
+
+// Dodaj klasu "active" na sledećeg igrača
+document.querySelector(`.child${currentPlayer}`).classList.toggle('active');
+document.querySelector(`.child${currentPlayer}`).classList.toggle('notactive');
+
+}
+
 
 //Add sum
 function addSum(){
@@ -89,13 +107,6 @@ function addSum(){
 
 }
 
-//Switch player
-function switchPlayer(){
-  currentPlayer = currentPlayer === 1 ? 2 : 1;
-  console.log(`Current player is: ${currentPlayer}`);
-}
-//Kad se klikne na hold, dodaj currentScore na global score
-//Kad se klikne na new game, resetuj sve na 0
 //Kad se dodje do 100, prikazi pobednika i onemoguci roll dice i hold
 
 });
